714c4e8897a64c4d02b05e6fa77b8ee511fe36e1
Andrey Maksimov <andrey@tenderowl.com>
2023-12-12 02:49:43 +0300

    Add re-send invite button

--- lib/app/modules/members/widgets/member_list_tile.dart
+++ lib/app/modules/members/widgets/member_list_tile.dart
@@ -1,6 +1,8 @@
 import 'package:appwrite/models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:get/get.dart';
 
 class MemberListTile extends StatelessWidget {
   const MemberListTile({
@@ -19,6 +21,33 @@ class MemberListTile extends StatelessWidget {
       ),
       title: Text(member.userName),
       subtitle: Text(member.userEmail),
+      trailing: SizedBox(
+        width: 160,
+        child: Row(
+          children: [
+            Chip(
+              label: Text(member.confirm ? 'Confirmed'.tr : 'Invited'.tr),
+              backgroundColor: member.confirm
+                  ? const Color(0xff87c271)
+                  : Theme.of(context).colorScheme.error,
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              side: BorderSide.none,
+              visualDensity: VisualDensity.compact,
+              onDeleted: member.confirm
+                  ? null
+                  : () {
+                      EasyLoading.showSuccess('Invite sent');
+                    },
+              deleteButtonTooltipMessage: 'Re-send invite',
+              deleteIcon: Icon(
+                TablerIcons.mail_fast,
+                size: 18,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+          ],
+        ),
+      ),
       onTap: () {
         EasyLoading.showInfo('Edit ${member.userName}');
       },
